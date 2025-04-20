5ad131ef7b0695846a7ee44569c836181679a8ec
ZhenShuo Leo <98386542+ZhenShuo2021@users.noreply.github.com>
2025-04-20 23:14:50 +0800

    disable plugins which requires compilers

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,9 +1,9 @@
 return {
   -- 禁用 LSP 相關插件
-  -- { "neovim/nvim-lspconfig", enabled = false },
-  -- { "williamboman/mason.nvim", enabled = false },
-  -- { "williamboman/mason-lspconfig.nvim", enabled = false },
-  -- { "folke/neodev.nvim", enabled = false },
+  { "neovim/nvim-lspconfig", enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
 
   -- 使用內建的 snacks explorer
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
@@ -21,9 +21,9 @@ return {
   { "Saghen/blink.cmp", enabled = false },
 
   -- 禁用需要編譯的插件，用於如TrueNAS等無法安裝C編譯器的機器
-  -- { "jose-elias-alvarez/null-ls.nvim", enabled = false },
-  -- { "nvim-treesitter/nvim-treesitter", enabled = false },
-  -- { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
-  -- { "telescope/telescope-fzf-native.nvim", enabled = false },
-  -- { "lewis6991/gitsigns.nvim", enabled = false },
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "telescope/telescope-fzf-native.nvim", enabled = false },
+  { "lewis6991/gitsigns.nvim", enabled = false },
 }
